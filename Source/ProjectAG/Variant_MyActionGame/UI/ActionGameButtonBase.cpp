4ada75a744a685ae843f566df4bf49ab5adc4d1e
Hyun Jae Cho <chohyunjae163@gmail.com>
2025-08-23 22:07:41 +0900

    -added an ActionGame button to setup text in a button

---
+++ Source/ProjectAG/Variant_MyActionGame/UI/ActionGameButtonBase.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ActionGameButtonBase.h"
+
+void UActionGameButtonBase::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	UpdateButtonText(ButtonText);
+}
---
+++ Source/ProjectAG/Variant_MyActionGame/UI/ActionGameButtonBase.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonButtonBase.h"
+#include "ActionGameButtonBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, MinimalAPI,Blueprintable)
+class UActionGameButtonBase : public UCommonButtonBase
+{
+	GENERATED_BODY()
+
+public:
+	// UUserWidget interface
+	virtual void NativePreConstruct() override;
+	// End of UUserWidget interface
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void UpdateButtonText(const FText& NewButtonText);
+	
+private:
+	UPROPERTY(EditAnywhere, Category="Button")
+	FText ButtonText;
+};
