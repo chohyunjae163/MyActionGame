f74593fa3d620a6d3197748e495dacb32fe7fa32
Hyun Jae Cho <chohyunjae163@gmail.com>
2025-09-08 12:06:02 +0900

    added weapon component and weapon definition
    weapon
    - damage
    - weight
    - static mesh

---
+++ Source/ProjectAG/Variant_MyActionGame/ActorComponent/WeaponComponent.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "WeaponComponent.generated.h"
+
+
+/**
+ * it holds a runtime character's weapon definition.
+ */
+UCLASS(MinimalAPI, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class UWeaponComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UWeaponComponent();
+
+private:
+	UPROPERTY(Transient)
+	TObjectPtr<class UWeaponDefinition> WeaponDef;
+};
---
+++ Source/ProjectAG/Variant_MyActionGame/Data/WeaponDefinition.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+
+#include "WeaponDefinition.generated.h"
+
+
+UCLASS(MinimalAPI,Blueprintable,Const)
+class UWeaponDefinition : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	FName Name;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<UStaticMesh> Mesh;
+
+	UPROPERTY(EditDefaultsOnly)
+	float BaseDamage;
+
+	UPROPERTY(EditDefaultsOnly)
+	float Weight;
+};
